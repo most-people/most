01aed61e074d950326fe73d0ddf843564d599c4f
大海 <iwangyang@qq.com>
2026-04-29 02:31:37 +0800

    Revert "feat: 新增纯前端+合约的 USDT 钱包充值提现功能"
    This reverts commit 3cf2d5c95e58e396604b659574812f6415fd2802.

--- app/wallet/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import WalletDashboard from '~/components/wallet/WalletDashboard'
-
-export default function WalletPage() {
-  return <WalletDashboard />
-}
